1a2bbd39ba0c12640e922eba52dca3c78fc4dd24
rodenhausen <thomas.rodenhausen@gmail.com>
2014-08-13 13:44:46 -0700

    updated group/package name

--- src/main/java/edu/arizona/biosemantics/oto2/oto/client/Oto.java
+++ src/main/java/edu/arizona/biosemantics/oto2/oto/client/Oto.java
@@ -1,18 +1,11 @@
 package edu.arizona.biosemantics.oto2.oto.client;
 
-import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.event.shared.EventBus;
 import com.google.gwt.event.shared.SimpleEventBus;
-import com.google.gwt.user.client.ui.RootPanel;
-import com.sencha.gxt.widget.core.client.container.Viewport;
 
 import edu.arizona.biosemantics.oto2.oto.client.categorize.CategorizePresenter;
 import edu.arizona.biosemantics.oto2.oto.client.categorize.CategorizeView;
 
-/**
- * Entry point classes define <code>onModuleLoad()</code>.
- */
-public class Oto implements EntryPoint {
+public class Oto {
 		
 	private SimpleEventBus eventBus;
 	private CategorizeView view;
@@ -23,24 +16,11 @@ public class Oto implements EntryPoint {
 		this.view = new CategorizeView(eventBus);
 		this.presenter = new CategorizePresenter(eventBus, view);
 		presenter.loadCollection(collectionId, secret);
-		
-
 	}
 	
 	public CategorizeView getView() {
 		return view;
 	}
 	
-	/**
-	 * This is the entry point method.
-	 */
-	public void onModuleLoad() {
-		int collectionId = 3;
-		String secret = "my secret";
-		
-		Oto oto = new Oto(collectionId, secret);
-		Viewport v = new Viewport();
-		v.add(oto.getView().asWidget());
-		RootPanel.get().add(v);
-	}
+
 }
---
+++ src/main/java/edu/arizona/biosemantics/oto2/oto/client/Start.java
@@ -0,0 +1,19 @@
+package edu.arizona.biosemantics.oto2.oto.client;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.ui.RootPanel;
+import com.sencha.gxt.widget.core.client.container.Viewport;
+
+public class Start implements EntryPoint {
+
+	public void onModuleLoad() {
+		int collectionId = 3;
+		String secret = "my secret";
+		
+		Oto oto = new Oto(collectionId, secret);
+		Viewport v = new Viewport();
+		v.add(oto.getView().asWidget());
+		RootPanel.get().add(v);
+	}
+
+}
