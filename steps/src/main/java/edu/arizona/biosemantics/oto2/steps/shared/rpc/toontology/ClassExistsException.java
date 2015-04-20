2fb15febee0e208126224413226b55a5513e1cfb
rodenhausen <thomas.rodenhausen@gmail.com>
2015-04-20 16:07:05 -0700

    model update: synonym does have entity, quality, and class does have class id; ontology dao refactoring

---
+++ steps/src/main/java/edu/arizona/biosemantics/oto2/steps/shared/rpc/toontology/ClassExistsException.java
@@ -0,0 +1,9 @@
+package edu.arizona.biosemantics.oto2.steps.shared.rpc.toontology;
+
+public class ClassExistsException extends Exception {
+
+	public ClassExistsException(String submissionTerm) {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ steps/src/main/java/edu/arizona/biosemantics/oto2/steps/shared/rpc/toontology/OntologyNotFoundException.java
@@ -0,0 +1,9 @@
+package edu.arizona.biosemantics.oto2.steps.shared.rpc.toontology;
+
+public class OntologyNotFoundException extends Exception {
+
+	public OntologyNotFoundException(String message) {
+		super(message);
+	}
+
+}
